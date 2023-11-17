98b00a5d6ebeec640410fba0b3c83616db9de1ac
Merima Dedic <merimaxdedic@gmail.com>
2023-11-16 20:09:12 -0600

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
-// code your solution here
+function superbowlWin(record) {
+    const winGame = record.find(game => game.result === "W");
+  
+    if (winGame) {
+      return winGame.year;
+    } else {
+      return undefined;
+    }
+  }
